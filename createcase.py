7aa460882b23bb820f723f502353e1bcd6ff853d
wangbinx <binx.a.wang@intel.com>
2018-06-30 17:18:21 +0800

    20180630 update
    Signed-off-by: wangbinx <binx.a.wang@intel.com>

--- createcase.py
+++ createcase.py
@@ -125,51 +125,147 @@ class create_case(config):
 	def all_case(self):
 		for key in self.caseinfo.keys():
 			case = self.caseinfo[key]
-			self.run_case(case)
+			result = self.run_case(case)
+			self.caseinfo[key].update(result)
+		return self.caseinfo
 
 	def run_case(self,case_dict):
-		msg = re.compile(r'%s'%case_dict['expected_result'])
-		try:
-			if case_dict['expected_result'] not in ['','N/A']:
-				result = subprocess.Popen('python %s %s' % (self.script_path, case_dict['case_command']),
-				                          stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-				out = result.stdout.read()
-				err = result.stderr.read()
+		if case_dict['expected_result'] not in ['', 'N/A']:
+			if '|' not in case_dict['expected_result']:
+				expcted_result = case_dict['expected_result']
+				command = case_dict['case_command']
+				msg = re.compile(r'%s'%expcted_result)
+				out,err = self.process(command)
 				if os.path.isfile(os.path.join(self.mould_file_path,case_dict['expected_result'])):
-					if out ==  self.read_module(os.path.join(self.mould_file_path,case_dict['expected_result'])):
-						case_dict['case_result'] = 'PASS'
-						print case_dict['case_id']
+					if out ==  self.read_module_txt_file(os.path.join(self.mould_file_path,case_dict['expected_result'])):
+						case_dict['case_result'] = 'Pass'
+	#					print case_dict['case_id']
 					else:
 						case_dict['case_result'] = 'Fail'
 				else:
 					if err:
 						if msg.search(err):
-							case_dict['case_result'] = 'PASS'
-							print case_dict['case_id']
+							case_dict['case_result'] = 'Pass'
+	#		    			print case_dict['case_id']
 						else:
 							print "%s fail message not match expected_result"%case_dict['case_id']
 							case_dict['case_result'] = 'Fail'
 			else:
-				print "%s not have expected result"%(case_dict['case_id'])
-			return case_dict
-		except Exception,e:
-			print e
+				result = False
+				expcted_result = case_dict['expected_result'].split('|')[-1]
+				command, value = self.parser_command(case_dict['case_command'])
+				print case_dict['case_id']
+				if '-o' in value.keys():
+					if os.path.exists(os.path.join(self.generatecapsule_path,value['-o'])):
+						os.remove(value['-o'])
+					run_out, run_err = self.process(command)
+					if run_err:
+						OUT = run_err
+						result = False
+					else:
+						info_out,info_err = self.process('%s --dump-info'%value['-o'])
+						OUT =(run_out+run_err+info_out+info_err).replace(' ','')
+						for v in value.keys():
+							if v in ['--capflag','--capoemflag','--guid','--hardware-instance','--monotonic-count','--version','--lsv','--debug']:
+								if v == '--guid':
+									tmp = self._search_info(self.command_dict[v], OUT, value[v].upper())
+								elif v == '--capflag':
+									tmp = self._search_info(self.command_dict[v], OUT)
+								else:
+									tmp = self._search_info(self.command_dict[v],OUT,'%08X'%eval(value[v]))
+								result = tmp & True
+					if result == True:
+						case_dict['case_result'] = 'Pass'
+					else:
+						print "%s fail message: %s" % (case_dict['case_id'],OUT)
+						case_dict['case_result'] = 'Fail'
+		else:
+			pass
+		#	print "%s not have expected result" % (case_dict['case_id'])
+		return case_dict
 
-	def read_module(self,file):
+	def _search_info(self,name,content,value=None):
+		if value == None:
+			reg = re.compile(r'%s'%name)
+			if reg.search(content):
+				return True
+			else:
+				return False
+		else:
+			reg = re.compile(r'%s=%s'%(name,value))
+			if reg.search(content):
+				return True
+			else:
+				return False
+
+	def read_module_txt_file(self,file):
 		with open(file,'r') as mf:
 			read= mf.read()
 		return read
 
-class write_result(object):
+	def parser_command(self,command):
+		_command_with_arg = ['-o','--capflag','--capoemflag','--guid','--hardware-instance','--monotonic-count','--version','--lsv','--pfx-file','--signer-private-cert','--other-public-cert','--trusted-public-cert','--signing-tool-path','--debug']
+		_command_without_arg = ['-h','-e','-d','--dump-info','-v','-q']
+		#print command
+		dict = {}
+		tmp =[]
+		c_list = command.split(' ')
+		for c in c_list:
+			if c in _command_with_arg:
+				dict[c] = c_list[c_list.index(c)+1]
+				tmp.append(c)
+				tmp.append(c_list[c_list.index(c)+1])
+		other =[elem for elem in c_list if elem not in tmp and elem !='']
+		#print other
+		#print dict
+		return command,dict
+
+	def process(self,command):
+		try:
+			result = subprocess.Popen('python %s %s' % (self.script_path, command),stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			out = result.stdout.read()
+			err = result.stderr.read()
+			return out,err
+		except Exception,e:
+			print e
+
+class write_result(config):
 
 	def __init__(self):
-		pass
+		super(write_result, self).__init__()
+		self.report = openpyxl.load_workbook(self.report_file)
+		active = self.report.active
+		self.sheet = self.report[active.title]
 
-	def result_to_excel(self):
-		pass
+	def color(self,value):
+		if value == "Fail":
+			return styles.Font(color=styles.colors.RED)
+		elif value == "Pass":
+			return styles.Font(color=styles.colors.GREEN)
+		else:
+			return
+
+	def result_to_excel(self,dict):
+		for title in self.sheet[1]:
+			if title.value == 'ID':
+				ID_col = title.column
+			elif title.value == 'Result':
+				Rst_col = title.column
+			else:
+				ID_col = 'G'
+				Rst_col = "H"
+		for n in range(2,self.sheet.max_row+1):
+			if self.sheet['%s%d'%(ID_col,n)].value:
+				if self.sheet['%s%d'%(ID_col,n)].value == dict['case_id']:
+					self.sheet['%s%d' % (Rst_col, n)].value = dict['case_result']
+					self.sheet['%s%d' % (Rst_col, n)].font = self.color(self.sheet['%s%d' % (Rst_col, n)].value)
+
+	def save(self):
+		return self.report.save('test.xlsx')
 
 def main():
 	case = create_case()
+	write_test_result = write_result()
 	os.environ['PYTHONPATH'] = case.python_path
 	print 'Please select case to run:\n'
 	print '1. Run all test case\n'
@@ -179,7 +275,10 @@ def main():
 	input = int(raw_input())
 	if input == 1:
 		print "Run all"
-		case.all_case()
+		result = case.all_case()
+		for i in result.keys():
+			write_test_result.result_to_excel(result[i])
+		write_test_result.save()
 	elif input == 2:
 		print "Category"
 	elif input == 3:
@@ -192,10 +291,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-	#case = create_case()
-
-	#case.case_info()
-	#os.environ['PYTHONPATH'] = case.python_path
-	#dict= {'case_result': '', 'case_calssify': 'Basic_Function', 'case_location': (4, 6), 'case_name': 'Basic_Function_\
-	#  -o   _GenerateCapsule.py -o', 'case_id': 'TC-4', 'expected_result': 'GenerateCapsule: error: argument -o/--output: expected one argument', 'case_command': ' -o'}
-	#case.run_case(dict)
